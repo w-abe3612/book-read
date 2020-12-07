c57a04ce3bad5ea8d8e59536d015b606a140495f
w-abe3612 <ookanadamo3612@gmail.com>
2020-12-07 13:51:44 +0000

    add a route to handle an unknown page to show a 404 message

--- src/App.js
+++ src/App.js
@@ -1,69 +1,78 @@
 import React from 'react';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ListBooks from './ListBooks.js';
 import SearchBooks from './SearchBooks.js';
 
 class BooksApp extends React.Component {
 
   state = {
-    query:"",
+    query: "",
     booklists: [],
-    searchLists:[]
+    searchLists: []
   }
+
+  NoMatchPage = () => {
+    return (
+      <h3>404 - Not found</h3>
+    )
+  };
   changeShelf = (book, shelf) => {
     BooksAPI.update(book, shelf)
-    .then(() => {
+      .then(() => {
 
-      BooksAPI.getAll()
-      .then((books) => {
-        this.setState(() => ({
-          booklists:books
-        }))
+        BooksAPI.getAll()
+          .then((books) => {
+            this.setState(() => ({
+              booklists: books
+            }))
+          });
       });
-    });
-  }
+  };
 
   searchHandler = (query) => {
-    if( query.length > 0 ) {
+    if (query.length > 0) {
       BooksAPI.search(query)
-      .then((books) => {
-        let searchResult = books !== undefined?books:[]
-        this.setState({searchLists:searchResult.error === undefined?searchResult:[]});
-      })
-    }else{
-      this.setState({searchLists:[]});
+        .then((books) => {
+          let searchResult = books !== undefined ? books : []
+          this.setState({ searchLists: searchResult.error === undefined ? searchResult : [] });
+        })
+    } else {
+      this.setState({ searchLists: [] });
     }
-    this.setState({query:query});
+    this.setState({ query: query });
   }
 
   componentDidMount() {
-      BooksAPI.getAll()
+    BooksAPI.getAll()
       .then((books) => {
         this.setState(() => ({
-          booklists:books
+          booklists: books
         }));
       });
   }
-  
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => (
-          <ListBooks
-            booklists={this.state.booklists}
-            changeShelf={this.changeShelf}
-          />
-        )} />
-        <Route exact path="/serch" render={() => (
-          <SearchBooks
-            searchLists={this.state.searchLists}
-            booklists={this.state.booklists}
-            searchHandler={this.searchHandler}
-            changeShelf={this.changeShelf}
-          />
-        )} />
+        <Switch>
+          <Route exact path="/" render={() => (
+            <ListBooks
+              booklists={this.state.booklists}
+              changeShelf={this.changeShelf}
+            />
+          )} />
+          <Route exact path="/serch" render={() => (
+            <SearchBooks
+              searchLists={this.state.searchLists}
+              booklists={this.state.booklists}
+              searchHandler={this.searchHandler}
+              changeShelf={this.changeShelf}
+            />
+          )} />
+          <Route component={this.NoMatchPage} />
+        </Switch>
       </div>
     )
   }
